6b828aa22a16373d340405310af1f98abd929076
JiamingChan <1043490933@qq.com>
2017-11-14 14:32:37 +0800

    1432

--- main.c
+++ main.c
@@ -19,6 +19,8 @@ int main(){
 	arraylist_link_add(list,5);
 	arraylist_link_add(list,6);
 	arraylist_link_add(list,7);
+	arraylist_link_add(list,8);
+	arraylist_link_add(list,9);
 	arraylist_link_lookup(list);
 	arraylist_link_free(list);
 	return 0;
