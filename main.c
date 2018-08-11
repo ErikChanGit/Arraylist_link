b3d42495fccd02d5be9ef0656e3117d7d23729f9
admin <admin@10.1.101.43>
2018-08-11 09:23:40 +0800

    1

--- main.c
+++ main.c
@@ -13,11 +13,6 @@ int main(){
 
 	struct arraylist_link *list = arraylist_link_init();
 	arraylist_link_add(list,1);
-	arraylist_link_add(list,2);
-	arraylist_link_add(list,3);
-	arraylist_link_add(list,4);
-	arraylist_link_add(list,5);
-	arraylist_link_add(list,6);
 	arraylist_link_lookup(list);
 	arraylist_link_free(list);
 	return 0;
